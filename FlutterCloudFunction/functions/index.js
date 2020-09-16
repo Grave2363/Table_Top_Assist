5adf3f867726938fa2e3249aadd665bad47879f0
Angelo Nicotra <AGNicotra@student.fullsail.edu>
2020-09-16 11:45:41 -0400

    fixes to cloud script, with the trubble might stop with cloud functions

--- FlutterCloudFunction/functions/index.js
+++ FlutterCloudFunction/functions/index.js
@@ -1,32 +1,47 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+
 admin.initializeApp(functions.config().functions);
+
 var newData;
-exports.messageTrigger = functions.firestore.document('ChatRoom/{roomId}').onCreate(async (snapshot, context) =>
-{
-    if (snapshot.empty)
-    {
-        console.log('No device');
+
+exports.myTrigger = functions.firestore.document('ChatRoom/{roomId}').onCreate(async (snapshot, context) => {
+    //
+
+    if (snapshot.empty) {
+        console.log('No Devices');
         return;
     }
-    var tokens = ['];
-    newData = snapshot.data;
-    var payload =
-    {
-        notification:
-        {
-            title: { 'push title', body: 'push body', sound: 'default' },
-            data: {click_action: 'FLUTTER_NOTIFICATION_CLICK'  ,message: 'push message' }
 
-        }
+    newData = snapshot.data();
+
+    const deviceIdTokens = await admin
+        .firestore()
+        .collection('Users')
+        .get();
+
+    var tokens = [];
+
+    for (var token of deviceIdTokens.docs) {
+        tokens.push(token.data().DeviceId);
+    }
+    var payload = {
+        notification: {
+            title: 'Push Title',
+            body: 'Push Body',
+            sound: 'default',
+        },
+        data: {
+            push_key: 'Push Key Value',
+            key1: newData.data,
+        },
     };
-    try
-    {
+
+    try {
         const response = await admin.messaging().sendToDevice(tokens, payload);
-        console.log('Notification sent');
-    } catch (e)
-    {
-        console.log('Error in Sending');
+        console.log('Notification sent successfully');
+    } catch (err) {
+        console.log(err);
     }
 });
 // // Create and Deploy Your First Cloud Functions
