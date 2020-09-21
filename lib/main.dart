8ba9a6633e7ace57576c55a18a14e85aecbc4d91
Angelo Nicotra <AGNicotra@student.fullsail.edu>
2020-09-21 10:42:34 -0400

    change

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
     return StreamProvider<User>.value(
       value: AuthSer().user,
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: wrapper(),
       ),
     );
