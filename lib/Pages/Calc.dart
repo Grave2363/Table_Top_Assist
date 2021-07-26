31f677b1e6d9a2f18e5cd5d578b78b0184ce5708
Angelo Nicotra <AGNicotra@student.fullsail.edu>
2021-07-26 13:58:32 -0400

    Update Calc.dart
    finished ui, now to work on  functionality

--- lib/Pages/Calc.dart
+++ lib/Pages/Calc.dart
@@ -19,6 +19,16 @@ class Calc extends StatefulWidget {
 }
 
 class _CalcState extends State<Calc> {
+  int numOne;
+  int numTwo;
+  String hist;
+  String textToDis;
+  String res;
+  String operator;
+  void btnOnClick(String btnVal)
+  {
+    // toDo : add functionality
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,49 +42,67 @@ class _CalcState extends State<Calc> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
+            Container(
+              child: Padding(
+                padding: EdgeInsets.only(right: 12),
+                child: Text(
+                  '999 X 999', style: TextStyle(fontSize: 25, color: Colors.white60),
+                ),
+              ),
+              alignment: Alignment(1.0, 1.0),
+            ),
+            Container(
+              child: Padding(
+                padding: EdgeInsets.all(12),
+                child: Text(
+                  '999999', style: TextStyle(fontSize: 50, color: Colors.white),
+                ),
+              ),
+              alignment: Alignment(1.0, 1.0),
+            ) ,
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                calc_btn(text: 'AC', textSize: 24, fillColor: 0xFF8ac4d0),
-                calc_btn(text: 'C', textSize: 24, fillColor: 0xFF8ac4d0),
-                calc_btn(text: 'DEL', textSize: 20, fillColor: 0xFF8ac4d0),
-                calc_btn(text: '/', textSize: 24, fillColor: 0xFF8ac4d0),
+                calc_btn(text: 'AC', textSize: 24, fillColor: 0xFF8ac4d0, callback: btnOnClick,),
+                calc_btn(text: 'C', textSize: 24, fillColor: 0xFF8ac4d0, callback: btnOnClick),
+                calc_btn(text: 'DEL', textSize: 20, fillColor: 0xFF8ac4d0, callback: btnOnClick),
+                calc_btn(text: '/', textSize: 24, fillColor: 0xFF8ac4d0, callback: btnOnClick),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                calc_btn(text: '9', textSize: 24, fillColor: 0xFF000000),
-                calc_btn(text: '8', textSize: 24, fillColor: 0xFF000000),
-                calc_btn(text: '7', textSize: 24, fillColor: 0xFF000000),
-                calc_btn(text: 'X', textSize: 24, fillColor: 0xFF8ac4d0),
+                calc_btn(text: '9', textSize: 24, fillColor: 0xFF000000, callback: btnOnClick),
+                calc_btn(text: '8', textSize: 24, fillColor: 0xFF000000, callback: btnOnClick),
+                calc_btn(text: '7', textSize: 24, fillColor: 0xFF000000, callback: btnOnClick),
+                calc_btn(text: 'X', textSize: 24, fillColor: 0xFF8ac4d0, callback: btnOnClick),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                calc_btn(text: '6', textSize: 24, fillColor: 0xFF000000),
-                calc_btn(text: '5', textSize: 24, fillColor: 0xFF000000),
-                calc_btn(text: '4', textSize: 24, fillColor: 0xFF000000),
-                calc_btn(text: '-', textSize: 40, fillColor: 0xFF8ac4d0),
+                calc_btn(text: '6', textSize: 24, fillColor: 0xFF000000, callback: btnOnClick),
+                calc_btn(text: '5', textSize: 24, fillColor: 0xFF000000, callback: btnOnClick),
+                calc_btn(text: '4', textSize: 24, fillColor: 0xFF000000, callback: btnOnClick),
+                calc_btn(text: '-', textSize: 40, fillColor: 0xFF8ac4d0, callback: btnOnClick),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                calc_btn(text: '3', textSize: 24, fillColor: 0xFF000000),
-                calc_btn(text: '2', textSize: 24, fillColor: 0xFF000000),
-                calc_btn(text: '1', textSize: 24, fillColor: 0xFF000000),
-                calc_btn(text: '+', textSize: 24, fillColor: 0xFF8ac4d0),
+                calc_btn(text: '3', textSize: 24, fillColor: 0xFF000000, callback: btnOnClick),
+                calc_btn(text: '2', textSize: 24, fillColor: 0xFF000000, callback: btnOnClick),
+                calc_btn(text: '1', textSize: 24, fillColor: 0xFF000000, callback: btnOnClick),
+                calc_btn(text: '+', textSize: 24, fillColor: 0xFF8ac4d0, callback: btnOnClick),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                calc_btn(text: '+/-', textSize: 22, fillColor: 0xFF8ac4d0),
-                calc_btn(text: '0', textSize: 24, fillColor: 0xFF8ac4d0),
-                calc_btn(text: '00', textSize: 24, fillColor: 0xFF8ac4d0),
-                calc_btn(text: '=', textSize: 24, fillColor: 0xFF8ac4d0),
+                calc_btn(text: '+/-', textSize: 22, fillColor: 0xFF8ac4d0, callback: btnOnClick),
+                calc_btn(text: '0', textSize: 24, fillColor: 0xFF8ac4d0, callback: btnOnClick),
+                calc_btn(text: '00', textSize: 24, fillColor: 0xFF8ac4d0, callback: btnOnClick),
+                calc_btn(text: '=', textSize: 24, fillColor: 0xFF8ac4d0, callback: btnOnClick),
               ],
             ),
           ],
